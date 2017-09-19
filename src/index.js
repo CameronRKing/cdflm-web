f033fe63fd4d4ce39a3e3906ae7a3503b9a0ef92
Cameron King <cameronking42@gmail.com>
2017-09-19 14:43:55 -0500

    Updates index.js to reflect NDPSO's updated API.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const NDPSO = require('./js/NDPSO.js');
 const {SimpleFacilityProblem} = require('./js/SimpleFacilityProblem.js');
 const city1990 = require('./js/problems/city1990.json');
 
+window.ndpso = new NDPSO();
 window.problem = new SimpleFacilityProblem(city1990);
-window.ndpso = new NDPSO(problem);
 console.time('city1990');
-window.gBest = ndpso.optimize();
+window.gBest = ndpso.optimize(problem);
 console.timeEnd('city1990');
